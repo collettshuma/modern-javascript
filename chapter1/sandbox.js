f4f084282258a338a06a92a22679d75094e91d5b
collettshuma <collettshuma@gmail.com>
2022-09-06 18:51:00 +0200

    updated chapter1 - strings

---
+++ chapter1/sandbox.js
@@ -0,0 +1,20 @@
+//strings
+
+//string concatenation
+let firstName = 'mario';
+let lastName = 'luigi';
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[2]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+
