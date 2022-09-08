0f5eecd180343f72d21e76f9bdc713b953f4f5db
collettshuma <collettshuma@gmail.com>
2022-09-08 13:59:21 +0200

    lesson6 added logical operators

--- chapter1/lesson6/sandbox.js
+++ chapter1/lesson6/sandbox.js
@@ -21,3 +21,36 @@ if(password.length > 6){
 }else{
     console.log('password is to short');
 }
+
+//to check multiple conditions
+const passWord = "dsahha";
+
+if(passWord.length >= 12){
+    console.log('this password is might strong');
+}else if(passWord.length >= 8){
+    console.log('this password is fairly strong');
+}else {
+    console.log('this is weak password');
+}
+
+// LOGICAL OPERETORS
+//Checking 2 conditions in the same line. && used to check if both conditions are true, they have to be true to execute the first statement.Other than that the else statement executes
+/*
+const psswrd = 'p@ssword1234';
+
+if(psswrd.length >= 12 && psswrd.includes('@')){
+    console.log('that password is mighty strong');
+}else if(psswrd.length >= 8){
+    console.log('that password is long enough');
+}else {
+    console.log('password is not long enough');
+}
+*/
+
+const psswrd = 'p@ss';
+
+if(psswrd.length >= 8 || psswrd.includes('@')){
+    console.log('that password is long enough');
+}else {
+    console.log('password is not long enough');
+} 
