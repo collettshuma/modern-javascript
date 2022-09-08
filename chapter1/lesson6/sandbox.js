a43de3a894d1387c0ed917ac62a435664577e0f8
collettshuma <collettshuma@gmail.com>
2022-09-08 11:48:16 +0200

    lesson6 if else statements

---
+++ chapter1/lesson6/sandbox.js
@@ -0,0 +1,23 @@
+//IF STATEMENTS
+
+const age = 20;
+
+if(age <= 20){
+    console.log("you are still young");
+}
+
+const names = ['josh','mario', 'luigi'];
+
+if (names.length > 1){
+    console.log("yes that is true");
+}
+
+//else if
+
+const password = "p@ss";
+
+if(password.length > 6){
+    console.log('password is long enough');
+}else{
+    console.log('password is to short');
+}
