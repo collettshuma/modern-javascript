1d751802dcaed877ce93c1ac5d71750c7d1dc5b3
collettshuma <collettshuma@gmail.com>
2022-09-08 10:34:41 +0200

    lesson5 add while loop

--- chapter1/lesson5/sandbox.js
+++ chapter1/lesson5/sandbox.js
@@ -1,5 +1,17 @@
-const names = ['mario','luigi','thomas'];
+//FOR LOOP
+/*const names = ['mario','luigi','thomas'];
 
 for(let i = 0; i < names.length; ++i){
     console.log(names[i]);
+}*/
+
+//WHILE LOOP
+
+const names = ['shaun', 'mario', 'luigi'];
+
+let i = 0;
+
+while(i < names.length){
+    console.log(names[i]);
+    ++i;
 }
