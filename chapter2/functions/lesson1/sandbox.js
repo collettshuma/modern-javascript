a052ed700891670a2ba444733b42a1105d84136b
collettshuma <collettshuma@gmail.com>
2022-09-09 09:58:52 +0200

    lesson1 functions

---
+++ chapter2/functions/lesson1/sandbox.js
@@ -0,0 +1,17 @@
+//function declaration
+
+function greet(){
+    console.log('hello, there');
+}
+
+//function expression
+const speak = function(){
+   console.log('eita magents');
+};
+
+
+
+
+greet();
+greet();
+speak();
