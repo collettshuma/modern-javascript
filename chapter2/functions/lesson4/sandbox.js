df196b6315aaa5c74e496ead37621fab84bd32c9
collettshuma <collettshuma@gmail.com>
2022-09-09 16:14:25 +0200

    lesson4 methods and functions

---
+++ chapter2/functions/lesson4/sandbox.js
@@ -0,0 +1,14 @@
+let igama = 'mario';
+
+//functions
+
+const greet = () => 'hello';
+
+let  resultOne = greet();
+console.log(resultOne);
+
+//methods
+let resultTwo = igama.toUpperCase();
+console.log(resultTwo);
+
+//the two only differ in the way we call them
