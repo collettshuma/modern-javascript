f63e13d425f3a234ff6fe515e70049541a017124
collettshuma <collettshuma@gmail.com>
2022-09-09 14:24:37 +0200

    lesson3 arrow function

--- chapter2/functions/lesson3/sandbox.js
+++ chapter2/functions/lesson3/sandbox.js
@@ -1,3 +1,4 @@
+/*
 const calcArea = function(radius){
 return 3.14 * radius **2;
 }
@@ -8,15 +9,45 @@ console.log(area);
 //if there is one parameter we lose the braces()
 //if there is one return statement we lose the return keyword and the brackets{}
 
-const area1 = (radius) => {
+const calcArea = (radius) => {
     return 3.14 * radius **2;
 };
 
-const a = calcArea(5);
-console.log(a);
+const area = calcArea(5);
+console.log(area);
+*/
 
 //futher break it down
-const calArea2 = radius => 3.14 * radius **2;
+const calcArea = radius => 3.14 * radius **2;
 
-const a2 = calcArea(5);
-console.log(a2);
+const area = calcArea(5);
+console.log(area);
+
+//example2
+// practise arrow functions
+
+// const greet = function(){
+//   return 'hello, world';
+// }
+
+const greet = () => 'hello, world';
+
+//example 3
+// const bill = function(products, tax){
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
+
+const bill = (products,tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+             total += products[i] + products[i] * tax;
+           }
+           return total;
+};
+
+console.log(greet());
+console.log(bill([10,15,30], 0.2))
