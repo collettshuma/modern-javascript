40c8aa416f6c5204950c33172c3f5321b4565745
collettshuma <collettshuma@gmail.com>
2022-09-13 08:42:32 +0200

    lesson5 callbacks & foreach

--- chapter2/functions/lesson5/sandbox.js
+++ chapter2/functions/lesson5/sandbox.js
@@ -1,5 +1,5 @@
 //callbacks & foreach
-
+/*
 const myFunc = (callbackFunc) => {
     //do something
     let value = 50;
@@ -7,4 +7,17 @@ const myFunc = (callbackFunc) => {
 };
 
 myFunc(function(value){console.log(value);});
+*/
 
+// callbacks & forEach
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+const logPerson = (person, index) => {
+  console.log(`${index} - hello ${person}`);
+}
+
+// people.forEach(person => {
+//   console.log(`hello ${person}`);
+// });
+
+people.forEach(logPerson);
