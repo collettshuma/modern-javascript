67780f322bb234ddcc4693656d355208412f1135
collettshuma <collettshuma@gmail.com>
2022-11-02 18:11:36 +0200

    promises

---
+++ chapter8/lesson2/sandbox.js
@@ -0,0 +1,43 @@
+let cleanRoom = () => {
+   return new Promise((resolve,reject) => {
+    let isClean = true;
+    if(isClean){
+        resolve(`the room has been cleaned`);
+    }else{
+        reject(`the room is still dirty`);
+    }
+   });
+};
+
+let removeGarbage = (message) => {
+    return new Promise((resolve,reject) => {
+        let finishedCleaning = true;
+
+        if(finishedCleaning){
+            resolve(`${message} go ahead and remove the garbage`);
+        } else{
+            reject(`wait for cleaning to be done`);
+        }
+    });
+};
+
+let winIcecream = (message) => {
+    return new Promise((resolve,reject) => {
+        let removeGarbage = true;
+
+        if(removeGarbage){
+            resolve(`${message} you have won icecream`);
+        }else{
+            reject('you not getting the icecream');
+        }
+    });
+};
+
+cleanRoom().then((resolve) => {
+    return removeGarbage(resolve);
+}). then((resolve) => {
+    return winIcecream(resolve);
+}). then((resolve) => {
+    console.log(`finished ${resolve}`);
+})
+
