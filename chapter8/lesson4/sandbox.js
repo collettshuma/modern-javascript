2eed681107b3e9f322873408c726738fb60e03af
collettshuma <collettshuma@gmail.com>
2022-11-18 06:18:09 +0200

    async await

--- chapter8/lesson4/sandbox.js
+++ chapter8/lesson4/sandbox.js
@@ -1,5 +1,11 @@
-async function print(){
-  return "eating oranges";
-}
+//Async Await
+/* what we need to do is to set up all our code to a async function
+then chain all our promises uses the await keyword.Thus for its a smarter way of 
+dealing with promise chaining */
 
-print().then(response => console.log(response));
+
+const getTodos = async () => {
+
+};
+const test = getTodos();
+console.log(test);
