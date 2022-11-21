e6ff16da2ba60e0782a748dfd8c162803f75dae9
collettshuma <collettshuma@gmail.com>
2022-11-21 08:52:24 +0200

    object

--- chapter8/lesson5/sandbox.js
+++ chapter8/lesson5/sandbox.js
@@ -2,15 +2,31 @@ let user = {
     name: 'collet',
     address: '8 Marlothi street',
     email: 'collettshuma@gmail.com',
-    login: () => {
+    blogs: ['make pap and meeat','buy pizza instead'],
+    login: function() {
         console.log('the user is logged in')
     },
-    logout: () => {
+    logout: function() {
         console.log('user is logged out')
+    },
+    logBlogs: function () {
+        //console.log(this.blogs);
+        this.blogs.forEach((blog) => {
+            console.log(blog);
+        })
     }
 }
 //user.login();
-user.logout();
+//user.logout();
 //console.log(user);
 //console.log(user.name);
+//console.log(user.blogs);
+user.logBlogs();
+
+
+//use (this) when you want to use the property in a method of the same object
+
+
+
+
 
