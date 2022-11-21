38f9ee305547100184f92d4c3266ead6708c3538
collettshuma <collettshuma@gmail.com>
2022-11-21 08:23:29 +0200

    object

---
+++ chapter8/lesson5/sandbox.js
@@ -0,0 +1,16 @@
+let user = {
+    name: 'collet',
+    address: '8 Marlothi street',
+    email: 'collettshuma@gmail.com',
+    login: () => {
+        console.log('the user is logged in')
+    },
+    logout: () => {
+        console.log('user is logged out')
+    }
+}
+//user.login();
+user.logout();
+//console.log(user);
+//console.log(user.name);
+
