791071083c12d748f2235daa061299aa1d6832be
collettshuma <collettshuma@gmail.com>
2022-11-30 16:34:16 +0200

    map()

--- chapter8/lesson6/sandbox.js
+++ chapter8/lesson6/sandbox.js
@@ -4,7 +4,7 @@ let map = new Map();
 map.set("1","Durban");
 map.set("2","Joburg");
 map.set("3","Cape Town");
-
+/*
 console.log(map.values());
 console.log(map.keys());
 
@@ -16,4 +16,6 @@ console.log(map.get("1"));
 //print all the keys and values
 for(let [key,value] of map){
     console.log(key, ":", value); 
-}
+}*/
+
+map.forEach((value,key) => console.log(key, ":", value));
