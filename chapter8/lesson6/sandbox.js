d5eea7b1f118f2b660b898b14e36ff2ad4b582fa
collettshuma <collettshuma@gmail.com>
2022-11-30 16:28:29 +0200

    map()

--- chapter8/lesson6/sandbox.js
+++ chapter8/lesson6/sandbox.js
@@ -1,8 +1,19 @@
+//key-value realtionship
 let map = new Map();
 
 map.set("1","Durban");
-map.set("2","Joburb");
-map.set("1","Cape Town");
+map.set("2","Joburg");
+map.set("3","Cape Town");
 
 console.log(map.values());
 console.log(map.keys());
+
+//check whether we has a particular key
+console.log(map.has("1"));
+
+console.log(map.get("1"));
+
+//print all the keys and values
+for(let [key,value] of map){
+    console.log(key, ":", value); 
+}
